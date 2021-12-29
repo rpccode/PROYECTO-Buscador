8e6e002f082c338b324068a2018322465629c2dd
Rudy Casilla <rpccode@gmail.com>
2021-12-28 21:09:52 -0500

    llenar los select de años

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 //Variables
 const Resultados = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max =new Date().getFullYear();
+const min = max - 10;
+
+
 
 //eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos();
+    mostrarAutos();//Muesta loas autos al cargar
+
+    //llena los anos 
+    llenarSelec();
 });
 
 
@@ -21,4 +30,14 @@ function mostrarAutos() {
             `;
             resultado.appendChild(autoHTML);
         })
+}
+
+function llenarSelec(){
+        for (let i = max; i >= min ; i--) {
+            const opcion = document.createElement('option');
+            opcion.value = i;
+            opcion.textContent = i;
+            year.appendChild(opcion);//agrega llas oopciones de year al select
+            
+        }
 }
